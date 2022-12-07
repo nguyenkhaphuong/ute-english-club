31da79a69a71542ef785a4d2b9d803b650318f1a
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2022-12-07 22:38:33 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
 import About from "./pages/About";
-import Contact from "./pages/Contact/Contact";
+import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
 //import vocabulary pages
 import Vocabulary from "./pages/Vocabulary";
-import Greetings from "./pages/Vocabulary/Greetings";
-import Transportation from "./pages/Vocabulary/Transportation";
-import Computers from "./pages/Vocabulary/Computers";
-import Travelling from "./pages/Vocabulary/Travelling";
-import Food from "./pages/Vocabulary/Food";
-import Music from "./pages/Vocabulary/Music";
 
 //import grammar pages
 import Grammar from "./pages/Grammar";
-import PresentSimple from "./pages/Grammar/PresentSimple";
-import PastSimple from "./pages/Grammar/PastSimple";
-import PresentContinous from "./pages/Grammar/PresentContinous";
-import PresentPerfect from "./pages/Grammar/PresentPerfect";
-import IrregularVerbs from "./pages/Grammar/IrregularVerbs";
+import IrregularVerbs from "./pages/IrregularVerbs";
 
 //import quiz pages
 import VocabQuiz from "./pages/Quiz/VocabQuiz";
@@ -44,20 +34,9 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route path="vocabulary">
             <Route index={true} element={<Vocabulary />} />
-            <Route path="greetings" element={<Greetings />} />
-            <Route path="transportation" element={<Transportation />} />
-            <Route path="computers" element={<Computers />} />
-            <Route path="travelling" element={<Travelling />} />
-            <Route path="food" element={<Food />} />
-            <Route path="music" element={<Music />} />
           </Route>
           <Route path="grammar">
             <Route index={true} element={<Grammar />} />
-            <Route path="presentsimple" element={<PresentSimple />} />
-            <Route path="pastsimple" element={<PastSimple />} />
-            <Route path="presentcontinous" element={<PresentContinous />} />
-            <Route path="presentperfect" element={<PresentPerfect />} />
-            <Route path="irregularverbs" element={<IrregularVerbs />} />
           </Route>
           <Route path="quiz">
             <Route index={true} element={<Quiz />} />
@@ -65,6 +44,7 @@ function App() {
             <Route path="grammar" element={<GrammarQuiz />} />
           </Route>
           <Route path="about" element={<About />} />
+          <Route path="irregularverbs" element={<IrregularVerbs />} />
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
