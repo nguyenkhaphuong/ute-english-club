8b8b7df056b5561a86a5f4af4a4f112ef0aafd1a
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2022-12-13 14:36:27 +0700

    Update VocabQuiz.jsx and GrammarQuiz.jsx

--- src/pages/Quiz/GrammarQuiz.jsx
+++ src/pages/Quiz/GrammarQuiz.jsx
@@ -1,72 +1,24 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Accordion, Button } from "react-bootstrap";
+import { category } from "../../database/grammarQuizData";
+import { TabTitle } from "../../utils/GeneralFunctions";
 
 function GrammarQuiz() {
-  const questions = [
-    {
-      questionText: "Which one is correct for present perfect tense?",
-      answerOptions: [
-        { id: 1, answerText: "I love you!", isCorrect: false },
-        {
-          id: 2,
-          answerText: "I have been to America since 2016",
-          isCorrect: true,
-        },
-        { id: 3, answerText: "What did you do yesterday?", isCorrect: false },
-        { id: 4, answerText: "Lucky you!", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Present continous tense",
-      answerOptions: [
-        { id: 1, answerText: "S + be V-ing + O", isCorrect: true },
-        { id: 2, answerText: "S + will V + O", isCorrect: false },
-        { id: 3, answerText: "S + V (past simple) + O", isCorrect: false },
-        { id: 4, answerText: "S + be V (past participle)", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Phuong ______ at the cinema last night",
-      answerOptions: [
-        { id: 1, answerText: "be", isCorrect: false },
-        { id: 2, answerText: "will be", isCorrect: false },
-        { id: 3, answerText: "was", isCorrect: true },
-        { id: 4, answerText: "is", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "How often _____ you ______ go to school?",
-      answerOptions: [
-        { id: 1, answerText: "was...go", isCorrect: false },
-        { id: 2, answerText: "have...gone", isCorrect: false },
-        { id: 3, answerText: "did...went", isCorrect: false },
-        { id: 4, answerText: "do...go", isCorrect: true },
-      ],
-    },
-    {
-      questionText: "How long ______ you ______ English?",
-      answerOptions: [
-        { id: 1, answerText: "was...study", isCorrect: false },
-        { id: 2, answerText: "have...studied", isCorrect: true },
-        { id: 3, answerText: "did...studied", isCorrect: false },
-        { id: 4, answerText: "does...study", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Be quiet! He _________. (sleep)",
-      answerOptions: [
-        { id: 1, answerText: "is sleeping", isCorrect: true },
-        { id: 2, answerText: "was slept", isCorrect: false },
-        { id: 3, answerText: "is sleep", isCorrect: false },
-        { id: 4, answerText: "sleeps", isCorrect: false },
-      ],
-    },
-  ];
+  TabTitle("Grammar Quiz | UTE English Club");
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
+
+  const resetQuiz = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowScore(false);
+  };
+
+  const btnScrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
 
   const handleAnswerOptionClick = (isCorrect) => {
     if (isCorrect) {
@@ -74,7 +26,7 @@ function GrammarQuiz() {
     }
 
     const nextQuestion = currentQuestion + 1;
-    if (nextQuestion < questions.length) {
+    if (nextQuestion < category[0].questions.length) {
       setCurrentQuestion(nextQuestion);
     } else {
       setShowScore(true);
@@ -82,72 +34,105 @@ function GrammarQuiz() {
   };
   return (
     <>
+      <div
+        className="p-4"
+        style={{
+          backgroundColor: "#dbdbec",
+        }}
+      >
+        <h1 className="container-sm display-5 fw-bold">Grammar Quiz</h1>
+        <p className="container-sm">Review your grammar with quizzes</p>
+      </div>
       <div className="section p-4 mt-3">
+        <Button
+          className="btn-floating sticky-bottom btn-lg justify-content-center align-self-center rounded-circle"
+          style={{
+            position: "fixed",
+            right: "20px",
+            bottom: "50px",
+            width: "50px",
+            height: "50px",
+            background:
+              "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
+          }}
+          onClick={btnScrollToTop}
+        >
+          <span className="fas fa-arrow-up"></span>
+        </Button>
         <div className="container-sm">
-          <div className="shadow container-sm rounded-2 border p-5">
-            {showScore ? (
-              <div className="display-5 score-section">
-                You scored {score} out of {questions.length}
-                <br />
-                <Button
-                  className="rounded-2 shadow-sm p-2 mt-3 me-3 fw-bold"
-                  onClick={() => window.location.reload(false)}
-                  style={{
-                    background:
-                      "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
-                    fontSize: "20px",
-                  }}
-                >
-                  Reset Quiz
-                </Button>
-                <Button
-                  className="rounded-2 shadow-sm p-2 mt-3 me-3 fw-bold"
-                  as={Link}
-                  to="/quiz"
-                  style={{
-                    background:
-                      "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
-                    fontSize: "20px",
-                  }}
-                >
-                  Go Back
-                </Button>
+          {category &&
+            category.map((category) => (
+              <div key={category.id}>
+                <Accordion>
+                  <Accordion.Item className="shadow p-2" eventKey={category.id}>
+                    <Accordion.Header>
+                      <h5 className="fw-bold">{category.name}</h5>
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      <div className="shadow container-sm rounded-2 border p-5">
+                        {showScore ? (
+                          <div className="display-5 score-section">
+                            <p>
+                              You scored {score} out of{" "}
+                              {category.questions.length} correct
+                            </p>
+                            <Button
+                              className="rounded-2 shadow-sm p-2 mt-3 me-3 fw-bold"
+                              onClick={resetQuiz}
+                              style={{
+                                background:
+                                  "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
+                                fontSize: "20px",
+                              }}
+                            >
+                              Reset Quiz
+                            </Button>
+                          </div>
+                        ) : (
+                          <>
+                            <div className="question-section">
+                              <div className="fw-bold h3 question-count mb-2">
+                                <span>Question {currentQuestion + 1}</span>/
+                                {category.questions.length}
+                              </div>
+                              <div className="h5 question-text mb-2">
+                                {
+                                  category.questions[currentQuestion]
+                                    .questionText
+                                }
+                              </div>
+                            </div>
+                            <div className="answer-section mt-2">
+                              <p className="fw-bold">Choose your answer:</p>
+                              {category.questions[
+                                currentQuestion
+                              ].answerOptions.map((answerOption) => (
+                                <Button
+                                  key={answerOption.id}
+                                  className="w-100 rounded-2 shadow-sm p-2 mt-2 fw-bold"
+                                  onClick={() =>
+                                    handleAnswerOptionClick(
+                                      answerOption.isCorrect
+                                    )
+                                  }
+                                  style={{
+                                    background:
+                                      "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
+                                    fontSize: "20px",
+                                  }}
+                                >
+                                  {answerOption.answerText}
+                                </Button>
+                              ))}
+                            </div>
+                          </>
+                        )}
+                      </div>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
               </div>
-            ) : (
-              <>
-                <div className="question-section">
-                  <div className="fw-bold h3 question-count mb-2">
-                    <span>Question {currentQuestion + 1}</span>/
-                    {questions.length}
-                  </div>
-                  <div className="h5 question-text mb-2">
-                    {questions[currentQuestion].questionText}
-                  </div>
-                </div>
-                <div className="answer-section mt-2">
-                  <p className="fw-bold">Choose your answer:</p>
-                  {questions[currentQuestion].answerOptions.map(
-                    (answerOption) => (
-                      <Button
-                        key={answerOption.id}
-                        className="w-100 rounded-2 shadow-sm p-2 mt-2 fw-bold"
-                        onClick={() =>
-                          handleAnswerOptionClick(answerOption.isCorrect)
-                        }
-                        style={{
-                          background:
-                            "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
-                          fontSize: "20px",
-                        }}
-                      >
-                        {answerOption.answerText}
-                      </Button>
-                    )
-                  )}
-                </div>
-              </>
-            )}
-          </div>
+            ))}
         </div>
       </div>
     </>
--- src/pages/Quiz/VocabQuiz.jsx
+++ src/pages/Quiz/VocabQuiz.jsx
@@ -1,167 +1,24 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Accordion, Button } from "react-bootstrap";
+import { TabTitle } from "../../utils/GeneralFunctions";
+import { category } from "../../database/vocabQuizData";
 
 function VocabQuiz() {
-  const questions = [
-    {
-      questionText: "desktop",
-      image: "https://cdn-icons-png.flaticon.com/512/1865/1865273.png",
-      answerOptions: [
-        { id: 1, answerText: "máy tính xách tay", isCorrect: false },
-        { id: 2, answerText: "đĩa CD", isCorrect: false },
-        { id: 3, answerText: "máy tính để bàn", isCorrect: true },
-        { id: 4, answerText: "đĩa mềm", isCorrect: false },
-      ],
-    },
-    {
-      id: 2,
-      questionText: "xe hơi",
-      image: "https://cdn-icons-png.flaticon.com/512/741/741407.png",
-      answerOptions: [
-        { id: 1, answerText: "truck", isCorrect: false },
-        { id: 2, answerText: "car", isCorrect: true },
-        { id: 3, answerText: "bus", isCorrect: false },
-        { id: 4, answerText: "ferry", isCorrect: false },
-      ],
-    },
-    {
-      id: 3,
-      questionText: "món khai vị",
-      image: "https://cdn-icons-png.flaticon.com/512/2558/2558974.png",
-      answerOptions: [
-        { id: 1, answerText: "appetizer", isCorrect: true },
-        { id: 2, answerText: "pizza", isCorrect: false },
-        { id: 3, answerText: "truck", isCorrect: false },
-        { id: 4, answerText: "diet", isCorrect: false },
-      ],
-    },
-    {
-      id: 4,
-      questionText: "musical instrument",
-      image: "https://cdn-icons-png.flaticon.com/512/4419/4419474.png",
-      answerOptions: [
-        { id: 1, answerText: "xe máy", isCorrect: false },
-        { id: 2, answerText: "khai vị", isCorrect: false },
-        { id: 3, answerText: "sân bay", isCorrect: false },
-        { id: 4, answerText: "nhạc cụ", isCorrect: true },
-      ],
-    },
-    {
-      id: 5,
-      questionText: "nghệ sĩ",
-      image: "https://cdn-icons-png.flaticon.com/512/7417/7417828.png",
-      answerOptions: [
-        { id: 1, answerText: "drum", isCorrect: false },
-        { id: 2, answerText: "truck", isCorrect: false },
-        { id: 3, answerText: "artist", isCorrect: true },
-        { id: 4, answerText: "floppy disk", isCorrect: false },
-      ],
-    },
-    {
-      id: 6,
-      questionText: "hello",
-      image: "https://cdn-icons-png.flaticon.com/512/5129/5129597.png",
-      answerOptions: [
-        { id: 1, answerText: "xin chào", isCorrect: true },
-        { id: 2, answerText: "xe buýt", isCorrect: false },
-        { id: 3, answerText: "ăn kiêng", isCorrect: false },
-        { id: 4, answerText: "tạm biệt", isCorrect: false },
-      ],
-    },
-    {
-      id: 7,
-      questionText: "tạm biệt",
-      image: "https://cdn-icons-png.flaticon.com/512/4081/4081653.png",
-      answerOptions: [
-        { id: 1, answerText: "good morning", isCorrect: false },
-        { id: 2, answerText: "plane", isCorrect: false },
-        { id: 3, answerText: "goodbye", isCorrect: true },
-        { id: 4, answerText: "salad", isCorrect: false },
-      ],
-    },
-    {
-      id: 8,
-      questionText: "nghệ sĩ chơi guitar",
-      image: "https://cdn-icons-png.flaticon.com/512/3048/3048387.png",
-      answerOptions: [
-        { id: 1, answerText: "receptionist", isCorrect: false },
-        { id: 2, answerText: "guitarist", isCorrect: true },
-        { id: 3, answerText: "piano", isCorrect: false },
-        { id: 4, answerText: "laptop", isCorrect: false },
-      ],
-    },
-    {
-      id: 9,
-      questionText: "suitcase",
-      image: "https://cdn-icons-png.flaticon.com/512/1663/1663017.png",
-      answerOptions: [
-        { id: 1, answerText: "bãi biển", isCorrect: false },
-        { id: 2, answerText: "đàn piano", isCorrect: false },
-        { id: 3, answerText: "khai vị", isCorrect: false },
-        { id: 4, answerText: "vali xách tay", isCorrect: true },
-      ],
-    },
-    {
-      id: 10,
-      questionText: "airport",
-      image: "https://cdn-icons-png.flaticon.com/512/3127/3127367.png",
-      answerOptions: [
-        { id: 1, answerText: "trạm xe buýt", isCorrect: false },
-        { id: 2, answerText: "sân bay", isCorrect: true },
-        { id: 3, answerText: "đàn ghi-ta", isCorrect: false },
-        { id: 4, answerText: "thuyền", isCorrect: false },
-      ],
-    },
-    {
-      id: 11,
-      questionText: "bãi biển",
-      image: "https://cdn-icons-png.flaticon.com/512/2664/2664650.png",
-      answerOptions: [
-        { id: 2, answerText: "beach", isCorrect: true },
-        { id: 1, answerText: "airport", isCorrect: false },
-        { id: 3, answerText: "car", isCorrect: false },
-        { id: 4, answerText: "appetizer", isCorrect: false },
-      ],
-    },
-    {
-      id: 12,
-      questionText: "xe đạp",
-      image: "https://cdn-icons-png.flaticon.com/512/562/562008.png",
-      answerOptions: [
-        { id: 1, answerText: "bus", isCorrect: false },
-        { id: 2, answerText: "bicycle", isCorrect: true },
-        { id: 3, answerText: "train", isCorrect: false },
-        { id: 4, answerText: "truck", isCorrect: false },
-      ],
-    },
-    {
-      id: 13,
-      questionText: "ambulance",
-      image: "https://cdn-icons-png.flaticon.com/512/2869/2869684.png",
-      answerOptions: [
-        { id: 1, answerText: "xe buýt", isCorrect: false },
-        { id: 3, answerText: "xe tải", isCorrect: false },
-        { id: 2, answerText: "xe cứu thương", isCorrect: true },
-        { id: 4, answerText: "trạm xe buýt", isCorrect: false },
-      ],
-    },
-    {
-      id: 14,
-      questionText: "good morning",
-      image: "https://cdn-icons-png.flaticon.com/512/6744/6744598.png",
-      answerOptions: [
-        { id: 1, answerText: "tạm biệt", isCorrect: false },
-        { id: 3, answerText: "xin chào", isCorrect: false },
-        { id: 4, answerText: "bãi biển", isCorrect: false },
-        { id: 2, answerText: "chào buổi sáng", isCorrect: true },
-      ],
-    },
-  ];
+  TabTitle("Vocabulary Quiz | UTE English Club");
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
+
+  const resetQuiz = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowScore(false);
+  };
+
+  const btnScrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
 
   const handleAnswerOptionClick = (isCorrect) => {
     if (isCorrect) {
@@ -169,7 +26,7 @@ function VocabQuiz() {
     }
 
     const nextQuestion = currentQuestion + 1;
-    if (nextQuestion < questions.length) {
+    if (nextQuestion < category[0].questions.length) {
       setCurrentQuestion(nextQuestion);
     } else {
       setShowScore(true);
@@ -177,81 +34,120 @@ function VocabQuiz() {
   };
   return (
     <>
+      <div
+        className="p-4"
+        style={{
+          backgroundColor: "#dbdbec",
+        }}
+      >
+        <h1 className="container-sm display-5 fw-bold">Vocabulary Quiz</h1>
+        <p className="container-sm">Review your vocabulary with quizzes</p>
+      </div>
       <div className="section p-4 mt-3">
+        <Button
+          className="btn-floating sticky-bottom btn-lg justify-content-center align-self-center rounded-circle"
+          style={{
+            position: "fixed",
+            right: "20px",
+            bottom: "50px",
+            width: "50px",
+            height: "50px",
+            background:
+              "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
+          }}
+          onClick={btnScrollToTop}
+        >
+          <span className="fas fa-arrow-up"></span>
+        </Button>
         <div className="container-sm">
-          <div className="shadow container-sm rounded-2 border p-5">
-            {showScore ? (
-              <div className="display-5 score-section">
-                <p>
-                  You scored {score} out of {questions.length} correct
-                </p>
-                <Button
-                  className="rounded-2 shadow-sm p-2 mt-3 me-3 fw-bold"
-                  onClick={() => window.location.reload(false)}
-                  style={{
-                    background:
-                      "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
-                    fontSize: "20px",
-                  }}
-                >
-                  Reset Quiz
-                </Button>
-                <Button
-                  className="rounded-2 shadow-sm p-2 mt-3 me-3 fw-bold"
-                  as={Link}
-                  to="/quiz"
-                  style={{
-                    background:
-                      "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
-                    fontSize: "20px",
-                  }}
-                >
-                  Go Back
-                </Button>
+          {category &&
+            category.map((category) => (
+              <div key={category.id}>
+                <Accordion>
+                  <Accordion.Item className="shadow p-2" eventKey={category.id}>
+                    <Accordion.Header>
+                      <img
+                        style={{ width: "2em", marginRight: "1.2em" }}
+                        src={category.image}
+                        alt={category.name}
+                      />
+                      <h5 className="fw-bold">{category.name}</h5>
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      <div className="shadow container-sm rounded-2 border p-5">
+                        {showScore ? (
+                          <div className="display-5 score-section">
+                            <p>
+                              You scored {score} out of{" "}
+                              {category.questions.length} correct
+                            </p>
+                            <Button
+                              className="rounded-2 shadow-sm p-2 mt-3 me-3 fw-bold"
+                              onClick={resetQuiz}
+                              style={{
+                                background:
+                                  "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
+                                fontSize: "20px",
+                              }}
+                            >
+                              Reset Quiz
+                            </Button>
+                          </div>
+                        ) : (
+                          <>
+                            <div className="question-section">
+                              <div className="fw-bold h3 question-count mb-2">
+                                <span>Question {currentQuestion + 1}</span>/
+                                {category.questions.length}
+                              </div>
+                              <div className="h5 question-text mb-2">
+                                {
+                                  category.questions[currentQuestion]
+                                    .questionText
+                                }
+                                <br />
+                                <img
+                                  className="mx-auto d-block"
+                                  src={
+                                    category.questions[currentQuestion].image
+                                  }
+                                  alt="Info"
+                                  width={"125px"}
+                                  style={{ maxWidth: "40%" }}
+                                />
+                              </div>
+                            </div>
+                            <div className="answer-section mt-2">
+                              <p className="fw-bold">Choose your answer:</p>
+                              {category.questions[
+                                currentQuestion
+                              ].answerOptions.map((answerOption) => (
+                                <Button
+                                  key={answerOption.id}
+                                  className="w-100 rounded-2 shadow-sm p-2 mt-2 fw-bold"
+                                  onClick={() =>
+                                    handleAnswerOptionClick(
+                                      answerOption.isCorrect
+                                    )
+                                  }
+                                  style={{
+                                    background:
+                                      "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
+                                    fontSize: "20px",
+                                  }}
+                                >
+                                  {answerOption.answerText}
+                                </Button>
+                              ))}
+                            </div>
+                          </>
+                        )}
+                      </div>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
               </div>
-            ) : (
-              <>
-                <div className="question-section">
-                  <div className="fw-bold h3 question-count mb-2">
-                    <span>Question {currentQuestion + 1}</span>/
-                    {questions.length}
-                  </div>
-                  <div className="h5 question-text mb-2">
-                    {questions[currentQuestion].questionText}
-                    <br />
-                    <img
-                      className="mx-auto d-block"
-                      src={questions[currentQuestion].image}
-                      alt="Info"
-                      width={"125px"}
-                      style={{ maxWidth: "40%" }}
-                    />
-                  </div>
-                </div>
-                <div className="answer-section mt-2">
-                  <p className="fw-bold">Choose your answer:</p>
-                  {questions[currentQuestion].answerOptions.map(
-                    (answerOption) => (
-                      <Button
-                        key={answerOption.id}
-                        className="w-100 rounded-2 shadow-sm p-2 mt-2 fw-bold"
-                        onClick={() =>
-                          handleAnswerOptionClick(answerOption.isCorrect)
-                        }
-                        style={{
-                          background:
-                            "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
-                          fontSize: "20px",
-                        }}
-                      >
-                        {answerOption.answerText}
-                      </Button>
-                    )
-                  )}
-                </div>
-              </>
-            )}
-          </div>
+            ))}
         </div>
       </div>
     </>
