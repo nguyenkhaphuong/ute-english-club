c8efd6fdff508d479be3a87f5f2d74b60183a59e
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2022-12-13 15:09:15 +0700

    Add Quiz data

---
+++ src/database/grammarQuizData.js
@@ -0,0 +1,74 @@
+export const category = [
+  {
+    id: 1,
+    name: "Present simple",
+    questions: [
+      {
+        id: 1,
+        questionText: "What _______ he _______ ?",
+        answerOptions: [
+          { id: 1, answerText: "was...do", isCorrect: false },
+          { id: 2, answerText: "does...do", isCorrect: true },
+          { id: 3, answerText: "has...done", isCorrect: false },
+          { id: 4, answerText: "was...done", isCorrect: false },
+        ],
+      },
+      {
+        id: 2,
+        questionText: "He _______ soccer.",
+        answerOptions: [
+          { id: 1, answerText: "doesn't play", isCorrect: true },
+          { id: 2, answerText: "do play", isCorrect: false },
+          { id: 3, answerText: "play", isCorrect: false },
+          { id: 4, answerText: "playing", isCorrect: false },
+        ],
+      },
+      {
+        id: 3,
+        questionText: "I _______ pizza",
+        answerOptions: [
+          { id: 1, answerText: "likes", isCorrect: false },
+          { id: 2, answerText: "not liking", isCorrect: false },
+          { id: 3, answerText: "doesn't like", isCorrect: false },
+          { id: 4, answerText: "like", isCorrect: true },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Past simple",
+    questions: [
+      {
+        id: 1,
+        questionText: "You _______ your homework yesterday.",
+        answerOptions: [
+          { id: 1, answerText: "finishes", isCorrect: false },
+          { id: 2, answerText: "didn't finish", isCorrect: true },
+          { id: 3, answerText: "not be finished", isCorrect: false },
+          { id: 4, answerText: "was finish", isCorrect: false },
+        ],
+      },
+      {
+        id: 2,
+        questionText: "_______ he _______ watch the World Cup last night?",
+        answerOptions: [
+          { id: 1, answerText: "Did...watch", isCorrect: true },
+          { id: 2, answerText: "Was...watched", isCorrect: false },
+          { id: 3, answerText: "Do...watch", isCorrect: false },
+          { id: 4, answerText: "How...watching", isCorrect: false },
+        ],
+      },
+      {
+        id: 3,
+        questionText: "He _______ to the cinema yesterday.",
+        answerOptions: [
+          { id: 1, answerText: "is go", isCorrect: false },
+          { id: 2, answerText: "going", isCorrect: false },
+          { id: 3, answerText: "was gone", isCorrect: false },
+          { id: 4, answerText: "went", isCorrect: true },
+        ],
+      },
+    ],
+  },
+];
---
+++ src/database/vocabQuizData.js
@@ -0,0 +1,221 @@
+export const category = [
+  {
+    id: 1,
+    name: "Computers",
+    image: "https://cdn-icons-png.flaticon.com/512/2933/2933245.png",
+    questions: [
+      {
+        id: 1,
+        questionText: "desktop",
+        image: "https://cdn-icons-png.flaticon.com/512/1865/1865273.png",
+        answerOptions: [
+          { id: 1, answerText: "máy tính xách tay", isCorrect: false },
+          { id: 2, answerText: "đĩa CD", isCorrect: false },
+          { id: 3, answerText: "máy tính để bàn", isCorrect: true },
+          { id: 4, answerText: "đĩa mềm", isCorrect: false },
+        ],
+      },
+      {
+        id: 2,
+        questionText: "laptop",
+        image: "https://cdn-icons-png.flaticon.com/512/2645/2645163.png",
+        answerOptions: [
+          { id: 1, answerText: "máy tính xách tay", isCorrect: true },
+          { id: 2, answerText: "đĩa CD", isCorrect: false },
+          { id: 3, answerText: "bãi biển", isCorrect: false },
+          { id: 4, answerText: "xe hơi", isCorrect: false },
+        ],
+      },
+      {
+        id: 3,
+        questionText: "phần mềm",
+        image: "https://cdn-icons-png.flaticon.com/512/1163/1163463.png",
+        answerOptions: [
+          { id: 1, answerText: "disk", isCorrect: false },
+          { id: 2, answerText: "software", isCorrect: true },
+          { id: 3, answerText: "good morning", isCorrect: false },
+          { id: 4, answerText: "guitar", isCorrect: false },
+        ],
+      },
+      {
+        id: 4,
+        questionText: "ổ đĩa cứng",
+        image: "https://cdn-icons-png.flaticon.com/512/2729/2729164.png",
+        answerOptions: [
+          { id: 1, answerText: "floppy disk", isCorrect: false },
+          { id: 2, answerText: "hardware", isCorrect: false },
+          { id: 3, answerText: "hard disk", isCorrect: true },
+          { id: 4, answerText: "software", isCorrect: false },
+        ],
+      },
+      {
+        id: 5,
+        questionText: "phần cứng",
+        image: "https://cdn-icons-png.flaticon.com/512/2729/2729197.png",
+        answerOptions: [
+          { id: 1, answerText: "operating system", isCorrect: false },
+          { id: 2, answerText: "hard disk", isCorrect: false },
+          { id: 3, answerText: "hardware", isCorrect: true },
+          { id: 4, answerText: "desktop", isCorrect: false },
+        ],
+      },
+      {
+        id: 6,
+        questionText: "floppy disk",
+        image: "https://cdn-icons-png.flaticon.com/512/2493/2493373.png",
+        answerOptions: [
+          { id: 1, answerText: "ổ đĩa mềm", isCorrect: true },
+          { id: 2, answerText: "máy tính xách tay", isCorrect: false },
+          { id: 3, answerText: "phần cứng", isCorrect: false },
+          { id: 4, answerText: "đĩa CD", isCorrect: false },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Travelling",
+    image: "https://cdn-icons-png.flaticon.com/512/2028/2028382.png",
+    questions: [
+      {
+        id: 1,
+        questionText: "suitcase",
+        image: "https://cdn-icons-png.flaticon.com/512/1663/1663017.png",
+        answerOptions: [
+          { id: 1, answerText: "nhạc cụ", isCorrect: false },
+          { id: 2, answerText: "xe máy", isCorrect: false },
+          { id: 3, answerText: "máy tính để bàn", isCorrect: false },
+          { id: 4, answerText: "vali xách tay", isCorrect: true },
+        ],
+      },
+      {
+        id: 2,
+        questionText: "camp",
+        image: "https://cdn-icons-png.flaticon.com/512/2079/2079177.png",
+        answerOptions: [
+          { id: 1, answerText: "xe buýt", isCorrect: false },
+          { id: 2, answerText: "khu cắm trại", isCorrect: true },
+          { id: 3, answerText: "bãi biển", isCorrect: false },
+          { id: 4, answerText: "xe hơi", isCorrect: false },
+        ],
+      },
+      {
+        id: 3,
+        questionText: "kỳ nghỉ",
+        image: "https://cdn-icons-png.flaticon.com/512/3020/3020920.png",
+        answerOptions: [
+          { id: 1, answerText: "vacation", isCorrect: true },
+          { id: 2, answerText: "bicycle", isCorrect: false },
+          { id: 3, answerText: "musical instrument", isCorrect: false },
+          { id: 4, answerText: "appetizer", isCorrect: false },
+        ],
+      },
+      {
+        id: 4,
+        questionText: "xe hơi",
+        image: "https://cdn-icons-png.flaticon.com/512/741/741407.png",
+        answerOptions: [
+          { id: 1, answerText: "airport", isCorrect: false },
+          { id: 2, answerText: "car", isCorrect: true },
+          { id: 3, answerText: "suitcase", isCorrect: false },
+          { id: 4, answerText: "bus", isCorrect: false },
+        ],
+      },
+      {
+        id: 5,
+        questionText: "sân bay",
+        image: "https://cdn-icons-png.flaticon.com/512/3127/3127367.png",
+        answerOptions: [
+          { id: 1, answerText: "beach", isCorrect: false },
+          { id: 2, answerText: "camp", isCorrect: false },
+          { id: 3, answerText: "bus", isCorrect: false },
+          { id: 4, answerText: "airport", isCorrect: true },
+        ],
+      },
+      {
+        id: 6,
+        questionText: "hotel",
+        image: "https://cdn-icons-png.flaticon.com/512/1946/1946788.png",
+        answerOptions: [
+          { id: 1, answerText: "khách sạn", isCorrect: true },
+          { id: 2, answerText: "vali xách tay", isCorrect: false },
+          { id: 3, answerText: "đại lý lữ hành", isCorrect: false },
+          { id: 4, answerText: "thuyền", isCorrect: false },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Food",
+    image: "https://cdn-icons-png.flaticon.com/512/857/857755.png",
+    questions: [
+      {
+        id: 1,
+        questionText: "appetizer",
+        image: "https://cdn-icons-png.flaticon.com/512/857/857755.png",
+        answerOptions: [
+          { id: 1, answerText: "bánh pizza", isCorrect: false },
+          { id: 2, answerText: "món khai vị", isCorrect: true },
+          { id: 3, answerText: "ẩm thực", isCorrect: false },
+          { id: 4, answerText: "thức ăn nhanh", isCorrect: false },
+        ],
+      },
+      {
+        id: 2,
+        questionText: "camp",
+        image: "https://cdn-icons-png.flaticon.com/512/2079/2079177.png",
+        answerOptions: [
+          { id: 1, answerText: "xe buýt", isCorrect: false },
+          { id: 2, answerText: "khu cắm trại", isCorrect: true },
+          { id: 3, answerText: "bãi biển", isCorrect: false },
+          { id: 4, answerText: "xe hơi", isCorrect: false },
+        ],
+      },
+      {
+        id: 3,
+        questionText: "kỳ nghỉ",
+        image: "https://cdn-icons-png.flaticon.com/512/3020/3020920.png",
+        answerOptions: [
+          { id: 1, answerText: "vacation", isCorrect: true },
+          { id: 2, answerText: "bicycle", isCorrect: false },
+          { id: 3, answerText: "musical instrument", isCorrect: false },
+          { id: 4, answerText: "appetizer", isCorrect: false },
+        ],
+      },
+      {
+        id: 4,
+        questionText: "xe hơi",
+        image: "https://cdn-icons-png.flaticon.com/512/741/741407.png",
+        answerOptions: [
+          { id: 1, answerText: "airport", isCorrect: false },
+          { id: 2, answerText: "car", isCorrect: true },
+          { id: 3, answerText: "suitcase", isCorrect: false },
+          { id: 4, answerText: "bus", isCorrect: false },
+        ],
+      },
+      {
+        id: 5,
+        questionText: "sân bay",
+        image: "https://cdn-icons-png.flaticon.com/512/3127/3127367.png",
+        answerOptions: [
+          { id: 1, answerText: "beach", isCorrect: false },
+          { id: 2, answerText: "camp", isCorrect: false },
+          { id: 3, answerText: "bus", isCorrect: false },
+          { id: 4, answerText: "airport", isCorrect: true },
+        ],
+      },
+      {
+        id: 6,
+        questionText: "hotel",
+        image: "https://cdn-icons-png.flaticon.com/512/1946/1946788.png",
+        answerOptions: [
+          { id: 1, answerText: "khách sạn", isCorrect: true },
+          { id: 2, answerText: "vali xách tay", isCorrect: false },
+          { id: 3, answerText: "đại lý lữ hành", isCorrect: false },
+          { id: 4, answerText: "thuyền", isCorrect: false },
+        ],
+      },
+    ],
+  },
+];
