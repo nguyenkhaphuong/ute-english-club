98d65925ca73bbefe54a065d23661bbd930c806e
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2023-01-11 23:23:45 +0700

    Update Vocabulary Page

--- src/pages/Vocabulary.jsx
+++ src/pages/Vocabulary.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 
-import { Button, Accordion, Table } from "react-bootstrap";
+import { Button, Card, Row, Col } from 'react-bootstrap'
 
-import { vocabCollection } from "../../firebase";
-import { onSnapshot, orderBy, query } from "firebase/firestore";
+import { vocabCollection } from '../../firebase'
+import { onSnapshot, orderBy, query } from 'firebase/firestore'
 
-import { TabTitle } from "../utils/GeneralFunctions";
+import { TabTitle } from '../utils/GeneralFunctions'
 
-import { Howl } from "howler";
-import ScrollToTop from "../components/scrollToTop/ScrollToTop";
+import { Howl } from 'howler'
+import ScrollToTop from '../components/scrollToTop/ScrollToTop'
 
 function Vocabulary() {
-  TabTitle("Vocabulary | UTE English Club");
+  TabTitle('Vocabulary | UTE English Club')
 
-  const [vocabulary, setVocabulary] = useState([]);
-  const queryRef = query(vocabCollection, orderBy("id", "asc"));
+  const [vocabulary, setVocabulary] = useState([])
+  const queryRef = query(vocabCollection, orderBy('id', 'asc'))
   useEffect(() =>
     onSnapshot(queryRef, (snapshot) => {
       setVocabulary(
@@ -22,112 +22,67 @@ function Vocabulary() {
           return {
             id: doc.id,
             ...doc.data(),
-          };
+          }
         })
-      );
+      )
     })
-  );
+  )
 
   const SoundPlay = (src) => {
     const sound = new Howl({
       src,
       html5: true,
-    });
-    sound.play();
-  };
+    })
+    sound.play()
+  }
 
   return (
     <>
       <div
-        className="p-4"
+        className='p-4'
         style={{
-          backgroundColor: "#dbdbec",
-        }}
-      >
-        <h1 className="container-sm display-5 fw-bold">Vocabulary</h1>
-        <p className="container-sm">
+          backgroundColor: '#dbdbec',
+        }}>
+        <h1 className='container-sm display-5 fw-bold'>Vocabulary</h1>
+        <p className='container-sm'>
           Here are the list of topics that you want to learn
         </p>
       </div>
-      <div className="section my-3 px-4">
+      <div className='section my-3 px-4'>
         <ScrollToTop />
-        <div className="container-sm">
-          <Accordion>
+        <div className='container-sm'>
+          <Row className='g-4'>
             {vocabulary.map((vocab) => {
               return (
-                <div key={vocab.id}>
-                  <Accordion.Item className="shadow p-2" eventKey={vocab.id}>
-                    <Accordion.Header>
-                      <img
-                        style={{ width: "2em", marginRight: "1.2em" }}
-                        src={vocab.image}
-                        alt={vocab.title}
-                      />
-                      <h5 className="fw-bold">{vocab.title}</h5>
-                    </Accordion.Header>
-                    <Accordion.Body>
-                      <Table striped bordered hover responsive="sm">
-                        <thead
-                          style={{
-                            background:
-                              "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
-                            color: "white",
-                          }}
-                        >
-                          <tr>
-                            <th>Words</th>
-                            <th></th>
-                            <th>Images</th>
-                            <th>Phonetics</th>
-                            <th>Vietnamese Translation</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {vocab.words &&
-                            vocab.words?.map((word) => {
-                              return (
-                                <tr key={word.id}>
-                                  <td>{word.word}</td>
-                                  <td className="text-center">
-                                    <Button
-                                      className="rounded-1"
-                                      style={{
-                                        background:
-                                          "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
-                                      }}
-                                      onClick={() => SoundPlay(word.audio)}
-                                    >
-                                      <span className="fa fa-volume-up"></span>
-                                    </Button>
-                                  </td>
-                                  <td>
-                                    <img
-                                      className="mx-auto d-block"
-                                      src={word.image}
-                                      style={{
-                                        width: "2.5em",
-                                        maxWidth: "8em",
-                                      }}
-                                      alt="Transport"
-                                    />
-                                  </td>
-                                  <td>{word.phonetic}</td>
-                                  <td>{word.translation}</td>
-                                </tr>
-                              );
-                            })}
-                        </tbody>
-                      </Table>
-                    </Accordion.Body>
-                  </Accordion.Item>
-                </div>
-              );
+                <Col key={vocab.id} sm={12} md={6} lg={4} xl={3}>
+                  <Card className='shadow p-2 text-center'>
+                    <Card.Img
+                      variant='top'
+                      className='mx-auto d-block p-2'
+                      style={{ width: '50%' }}
+                      src={vocab.image}
+                    />
+                    <Card.Body>
+                      <Card.Title>{vocab.title}</Card.Title>
+                      <Card.Text>{vocab.description}</Card.Text>
+                      <Button
+                        style={{
+                          background:
+                            'linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)',
+                        }}
+                        href={`/vocabulary/${vocab.id}`}>
+                        <span className='fw-bold'>Start Learning</span>
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              )
             })}
-          </Accordion>
+          </Row>
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default Vocabulary;
+export default Vocabulary
