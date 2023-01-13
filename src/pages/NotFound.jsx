b3a4ee83e2f2ddef81d8652019b2de5afaf4b173
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2023-01-13 15:35:03 +0700

    Replace TabTitle with React Helmet

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,33 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
-import { TabTitle } from "../utils/GeneralFunctions";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+import { Helmet } from 'react-helmet-async'
 
 function NotFound() {
-  TabTitle("404 Not Found | UTE English Club");
   return (
-    <div className="not-found">
+    <div className='not-found'>
+      <Helmet>
+        <title>404 Not Found | UTE English Club</title>
+        <meta name='description' content='404 Not Found' />
+      </Helmet>
       <img
-        src="https://www.pngitem.com/pimgs/m/561-5616833_image-not-found-png-not-found-404-png.png"
-        alt="not-found"
-        className="img-fluid img-thumbnail mx-auto d-block"
+        src='https://www.pngitem.com/pimgs/m/561-5616833_image-not-found-png-not-found-404-png.png'
+        alt='not-found'
+        className='img-fluid img-thumbnail mx-auto d-block'
       />
       <hr />
       <center>
         <Button
           as={Link}
-          to="/"
-          className="fw-bold"
+          to='/'
+          className='fw-bold'
           style={{
             background:
-              "linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)",
-          }}
-        >
+              'linear-gradient(135deg, rgba(62,64,149,1) 50%, rgba(237,50,55,1) 100%)',
+          }}>
           Back to Homepage
         </Button>
       </center>
     </div>
-  );
+  )
 }
 
-export default NotFound;
+export default NotFound
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,83 +1,82 @@
-import React from "react";
-import { Button, Row, Col, Card } from "react-bootstrap";
+import React from 'react'
+import { Button, Row, Col, Card } from 'react-bootstrap'
 
-import { TabTitle } from "../utils/GeneralFunctions";
-import { Link } from "react-router-dom";
-import ScrollToTop from "../components/scrollToTop/ScrollToTop";
+import { Link, Outlet } from 'react-router-dom'
+import ScrollToTop from '../components/scrollToTop/ScrollToTop'
+import { Helmet } from 'react-helmet-async'
 
 function Quiz() {
-  TabTitle("Quiz | UTE English Club");
-
   return (
     <>
+      <Helmet>
+        <title>Quiz | UTE English Club</title>
+        <meta name='description' content='Review your lessons with quizzes' />
+      </Helmet>
       <div
-        className="p-4"
+        className='p-4'
         style={{
-          backgroundColor: "#dbdbec",
-        }}
-      >
-        <h1 className="container-sm display-5 fw-bold">Quiz</h1>
-        <p className="container-sm">Review your lessons with quizzes</p>
+          backgroundColor: '#dbdbec',
+        }}>
+        <h1 className='container-sm display-5 fw-bold'>Quiz</h1>
+        <p className='container-sm'>Review your lessons with quizzes</p>
       </div>
-      <div className="section mt-3 px-4">
+      <div className='section mt-3 px-4'>
         <ScrollToTop />
-        <div className="container-sm">
-          <Row className="mt-1 gy-4">
+        <div className='container-sm'>
+          <Row className='mt-1 gy-4'>
             <Col md={6}>
-              <Card className="shadow" style={{ maxWidth: "100%" }}>
+              <Card className='shadow' style={{ maxWidth: '100%' }}>
                 <Card.Img
-                  className="p-3 mx-auto d-block"
-                  variant="top"
-                  style={{ width: "30%" }}
-                  src="https://cdn-icons-png.flaticon.com/512/1902/1902654.png"
+                  className='p-3 mx-auto d-block'
+                  variant='top'
+                  style={{ width: '30%' }}
+                  src='https://cdn-icons-png.flaticon.com/512/1902/1902654.png'
                 />
-                <Card.Body style={{ backgroundColor: "#e1e1e1" }}>
-                  <Card.Title className="text-center fw-bold">
+                <Card.Body style={{ backgroundColor: '#e1e1e1' }}>
+                  <Card.Title className='text-center fw-bold'>
                     Vocabulary
                   </Card.Title>
-                  <Card.Text className="text-center">
+                  <Card.Text className='text-center'>
                     Review new words you have learned
                   </Card.Text>
                 </Card.Body>
                 <Button
                   as={Link}
-                  to="/quiz/vocabulary"
-                  className="fw-bold"
+                  to='/quiz/vocabulary'
+                  className='fw-bold'
                   style={{
                     background:
-                      "linear-gradient(135deg, rgba(62,64,149,1) 55%, rgba(237,50,55,1) 100%)",
-                  }}
-                >
+                      'linear-gradient(135deg, rgba(62,64,149,1) 55%, rgba(237,50,55,1) 100%)',
+                  }}>
                   Start Practicing
                 </Button>
               </Card>
             </Col>
 
             <Col md={6}>
-              <Card className="shadow" style={{ maxWidth: "100%" }}>
+              <Card className='shadow' style={{ maxWidth: '100%' }}>
                 <Card.Img
-                  className="p-3 mx-auto d-block"
-                  variant="top"
-                  style={{ width: "30%" }}
-                  src="https://cdn-icons-png.flaticon.com/512/6091/6091117.png"
+                  className='p-3 mx-auto d-block'
+                  variant='top'
+                  style={{ width: '30%' }}
+                  src='https://cdn-icons-png.flaticon.com/512/6091/6091117.png'
                 />
-                <Card.Body style={{ backgroundColor: "#e1e1e1" }}>
-                  <Card.Title className="text-center fw-bold">
+                <Card.Body style={{ backgroundColor: '#e1e1e1' }}>
+                  <Card.Title className='text-center fw-bold'>
                     Grammar
                   </Card.Title>
-                  <Card.Text className="text-center">
+                  <Card.Text className='text-center'>
                     Review the basic grammar
                   </Card.Text>
                 </Card.Body>
                 <Button
                   as={Link}
-                  to="/quiz/grammar"
-                  className="fw-bold"
+                  to='/quiz/grammar'
+                  className='fw-bold'
                   style={{
                     background:
-                      "linear-gradient(135deg, rgba(62,64,149,1) 55%, rgba(237,50,55,1) 100%)",
-                  }}
-                >
+                      'linear-gradient(135deg, rgba(62,64,149,1) 55%, rgba(237,50,55,1) 100%)',
+                  }}>
                   Start Practicing
                 </Button>
               </Card>
@@ -85,8 +84,9 @@ function Quiz() {
           </Row>
         </div>
       </div>
+      <Outlet />
     </>
-  );
+  )
 }
 
-export default Quiz;
+export default Quiz
