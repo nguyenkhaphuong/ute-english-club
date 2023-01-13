df14c6642297d298a3c0fc5f64532ea202ec848c
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2023-01-13 15:33:25 +0700

    Add Dynamic Routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 import Topbar from './components/topbar/Topbar'
 import Footer from './components/footer/Footer'
 import { Routes, Route } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { HelmetProvider } from 'react-helmet-async'
 
 //import main pages
 import Home from './pages/Home'
@@ -16,44 +16,54 @@ import NotFound from './pages/NotFound'
 
 //import vocabulary pages
 import Vocabulary from './pages/Vocabulary'
+import VocabularyTopic from './pages/VocabularyTopic'
 
 //import grammar pages
 import Grammar from './pages/Grammar'
 import IrregularVerbs from './pages/IrregularVerbs'
+import GrammarTopic from './pages/GrammarTopic'
 
 //import quiz pages
 import VocabQuiz from './pages/Quiz/VocabQuiz'
+import VocabQuizTopic from './pages/Quiz/VocabQuizTopic'
 import GrammarQuiz from './pages/Quiz/GrammarQuiz'
-import GrammarTopic from './pages/GrammarTopic'
-import VocabularyTopic from './pages/VocabularyTopic'
+import GrammarQuizTopic from './pages/Quiz/GrammarQuizTopic'
 
 function App() {
   return (
     <div className='App'>
-      <Topbar />
-      <div className='sections'>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='vocabulary'>
-            <Route index={true} element={<Vocabulary />} />
-            <Route path=':name' element={<VocabularyTopic />} />
-          </Route>
-          <Route path='grammar'>
-            <Route index={true} element={<Grammar />} />
-            <Route path=':name' element={<GrammarTopic />} />
-          </Route>
-          <Route path='quiz'>
-            <Route index={true} element={<Quiz />} />
-            <Route path='vocabulary' element={<VocabQuiz />} />
-            <Route path='grammar' element={<GrammarQuiz />} />
-          </Route>
-          <Route path='about' element={<About />} />
-          <Route path='irregularverbs' element={<IrregularVerbs />} />
-          <Route path='contact' element={<Contact />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </div>
-      <Footer />
+      <HelmetProvider>
+        <Topbar />
+        <div className='sections'>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='vocabulary'>
+              <Route index element={<Vocabulary />} />
+              <Route path=':title' element={<VocabularyTopic />} />
+            </Route>
+            <Route path='grammar'>
+              <Route index element={<Grammar />} />
+              <Route path=':title' element={<GrammarTopic />} />
+            </Route>
+            <Route path='quiz'>
+              <Route index element={<Quiz />} />
+              <Route path='vocabulary'>
+                <Route index element={<VocabQuiz />} />
+                <Route path=':name' element={<VocabQuizTopic />} />
+              </Route>
+              <Route path='grammar'>
+                <Route index element={<GrammarQuiz />} />
+                <Route path=':name' element={<GrammarQuizTopic />} />
+              </Route>
+            </Route>
+            <Route path='about' element={<About />} />
+            <Route path='irregularverbs' element={<IrregularVerbs />} />
+            <Route path='contact' element={<Contact />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </div>
+        <Footer />
+      </HelmetProvider>
     </div>
   )
 }
