14cb9b8c964ca21a0c92b696025e94b8a3b3240f
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2023-07-06 20:58:28 +0700

    Create Dynamic Year Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import "./Footer.scss";
+import React from 'react'
+import './Footer.scss'
 
 function Footer() {
+  const year = new Date().getFullYear()
   return (
     <footer
-      style={{ fontSize: "14px" }}
-      className="p-2 text-center fw-bold fixed-bottom"
-    >
-      &copy; 2022 UTE English Club. All rights reserved.
+      style={{ fontSize: '14px' }}
+      className='p-2 text-center fw-bold fixed-bottom'>
+      &copy; {year} UTE English Club. All rights reserved.
     </footer>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
