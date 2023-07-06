bae34bf0c928ccfe193d823dba4a673682c3f379
Nguyễn Khả Phương <phuongkha1506@gmail.com>
2023-07-06 20:47:31 +0700

    Update Image Links in Slider

--- src/components/imageslider/ImageSlider.jsx
+++ src/components/imageslider/ImageSlider.jsx
@@ -1,5 +1,8 @@
-import React from "react";
-import Carousel from "react-bootstrap/Carousel";
+import React from 'react'
+import Carousel from 'react-bootstrap/Carousel'
+import Image1 from '../../images/students.png'
+import Image2 from '../../images/students2.png'
+import Image3 from '../../images/students3.png'
 
 function ImageSlider() {
   return (
@@ -7,52 +10,49 @@ function ImageSlider() {
       <Carousel controls={false} fade>
         <Carousel.Item interval={3000}>
           <img
-            className="img-responsive d-block w-100"
-            src="src\images\students.jpg"
-            alt="First slide"
-            style={{ width: "50%" }}
+            className='img-responsive d-block w-100'
+            src={Image1}
+            alt='First slide'
+            style={{ width: '50%' }}
           />
           <Carousel.Caption
-            className="d-flex flex-column justify-content-center h-100"
-            style={{ top: "0" }}
-          >
-            <h1 className="fw-bold">Welcome to UTE English Club</h1>
+            className='d-flex flex-column justify-content-center h-100'
+            style={{ top: '0' }}>
+            <h1 className='fw-bold'>Welcome to UTE English Club</h1>
             <p>Learn English anytime, anywhere. 100% FREE</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={3000}>
           <img
-            className="img-responsive d-block w-100"
-            src="src\images\students2.jpg"
-            alt="Second slide"
-            style={{ width: "50%" }}
+            className='img-responsive d-block w-100'
+            src={Image2}
+            alt='Second slide'
+            style={{ width: '50%' }}
           />
           <Carousel.Caption
-            className="d-flex flex-column justify-content-center h-100"
-            style={{ top: "0" }}
-          >
-            <h1 className="fw-bold">Welcome to UTE English Club</h1>
+            className='d-flex flex-column justify-content-center h-100'
+            style={{ top: '0' }}>
+            <h1 className='fw-bold'>Welcome to UTE English Club</h1>
             <p>Learn English anytime, anywhere. 100% FREE</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={3000}>
           <img
-            className="img-responsive d-block w-100"
-            src="src\images\students3.jpg"
-            alt="Third slide"
-            style={{ width: "50%" }}
+            className='img-responsive d-block w-100'
+            src={Image3}
+            alt='Third slide'
+            style={{ width: '50%' }}
           />
           <Carousel.Caption
-            className="d-flex flex-column justify-content-center h-100"
-            style={{ top: "0" }}
-          >
-            <h1 className="fw-bold">Welcome to UTE English Club</h1>
+            className='d-flex flex-column justify-content-center h-100'
+            style={{ top: '0' }}>
+            <h1 className='fw-bold'>Welcome to UTE English Club</h1>
             <p>Learn English anytime, anywhere. 100% FREE</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </>
-  );
+  )
 }
 
-export default ImageSlider;
+export default ImageSlider
